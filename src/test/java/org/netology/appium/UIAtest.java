c4396cb2b623bc9f69fb61fafc0d35eef4a39b8f
Alexander Mizyukin <a.mizyukin@mail.ru>
2022-01-17 17:59:02 +0300

    first ver

--- src/test/java/org/netology/appium/UIAtest.java
+++ src/test/java/org/netology/appium/UIAtest.java
@@ -1,14 +1,21 @@
 package org.netology.appium;
 
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 
 public class UIAtest {
 
@@ -19,25 +26,53 @@ public class UIAtest {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("platformName", "Android");
         desiredCapabilities.setCapability("appium:deviceName", "Some name");
+//        desiredCapabilities.setCapability("deviceName", "emulator-5556" );
+//        desiredCapabilities.setCapability("appium:app", "C:\\Users\\Admin\\IdeaProjects\\netology-appium-UIautomator\\app\\build\\outputs\\apk\\androidTest\\debug\\app-debug-androidTest.apk");
         desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
         desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
         desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
         desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
 
-        URL remoteUrl = new URL("http://127.0.0.1:4723/wd/hub");
+        URL remoteUrl = new URL("http://localhost:4723/wd/hub");
 
         driver = new AndroidDriver(remoteUrl, desiredCapabilities);
     }
 
     @Test
-    public void sampleTest() {
-        MobileElement el1 = (MobileElement) driver.findElementByAccessibilityId("UiAutomator");
-        el1.click();
-        MobileElement el4 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/userInput");
-        el4.sendKeys("                                  ");
-        MobileElement el5 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/buttonActivity");
-        el5.click();
+    public void testInputSpaces() {
+
+        MobileElement el2 = (MobileElement) driver.findElementByAccessibilityId("UiAutomator");
+        el2.click();
+        MobileElement textBefore = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/textToBeChanged");
+        MobileElement el3 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/userInput");
+        el3.sendKeys("    ");
+        MobileElement el4 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/buttonChange");
+        el4.click();
+        MobileElement textAfter = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/textToBeChanged");
+        assertEquals(textBefore, textAfter);
     }
+
+    @Test
+    public void testInputNone() {
+
+        MobileElement textBefore = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/textToBeChanged");
+        MobileElement el2 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/buttonChange");
+        el2.click();
+        MobileElement textAfter = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/textToBeChanged");
+        assertEquals(textBefore, textAfter);
+    }
+
+    @Test
+    public void testChangeTextAndNewActivity() {
+        MobileElement el1 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/userInput");
+        el1.sendKeys("Hallo");
+        MobileElement el2 = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/buttonActivity");
+        el2.click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        MobileElement textAfter = (MobileElement) driver.findElementById("ru.netology.testing.uiautomator:id/text");
+        assertEquals("Hallo", textAfter.getText());
+    }
+
 
     @AfterAll
     public void tearDown() {
